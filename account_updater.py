fc1a2a1905e7a9f6b4c0ec234ba92412490bbb21
pengtb <ptbjj@126.com>
2025-09-11 19:57:02 +0800

    unify name & format

---
+++ account_updater.py
@@ -0,0 +1,70 @@
+from api.account import Account_API
+from importer.eaccount import EAccountImporter
+from importer.alipayfund import AlipayFundImporter
+import argparse
+from tqdm import tqdm
+import pandas as pd
+
+def parse_args(cmdline=None):
+    parser = argparse.ArgumentParser(description="ezBookkeeping Account Updater")
+    parser.add_argument("--file", type=str, help="Path to the input file")
+    parser.add_argument("--action", type=str, help="Action to perform", choices=["add", "list", "delete", "modify"])
+    parser.add_argument("--importer", type=str, help="Importer to use", choices=["eaccount", "alipay"], default="eaccount")
+    parser.add_argument("--update-info", action='store_true')
+    args = parser.parse_args(cmdline)
+    return args
+
+if __name__ == "__main__":
+    # parse arguments
+    args = parse_args()
+    
+    # create api
+    api = Account_API()
+    
+    # create importer
+    if args.importer == "eaccount":
+        importer = EAccountImporter()
+        query_accounts = importer.import_accounts(args.file, update_info=args.update_info)
+        
+    elif args.importer == "alipay":
+        importer = AlipayFundImporter()
+        query_accounts = importer.import_accounts(args.file, update_info=args.update_info)
+            
+    # add accounts
+    if args.action == "add":
+        for account in tqdm(query_accounts):
+            response = api.add_account(**account.to_dict())
+            if response["success"] != True: 
+                print(account.to_dict())
+                raise Exception(response)
+        print(f"Added {len(query_accounts)} accounts")
+        
+    # list & modify & delete accounts
+    elif (args.action == "list") or (args.action == "modify") or (args.action == "delete"):
+        response = api.list_accounts()
+        ## convert to dataframe
+        result = response['result']
+        result_df = pd.DataFrame(result)
+        
+        # list accounts
+        if (args.action == "list"):
+            print(result_df.loc[:, ['name', 'balance', 'currency']])
+            
+        elif (args.action == "modify") or (args.action == "delete"):
+            ## filter accounts according to query
+            query_df = pd.DataFrame([account.to_dict() for account in query_accounts])
+            ## merge dataframes
+            merged_df = pd.merge(result_df.loc[:, ['id', 'name']], query_df, on='name', how='inner')
+            
+            # modify accounts
+            if (args.action == "modify"):
+                for id in tqdm(merged_df['id']):
+                    api.modify_account(**merged_df[merged_df['id'] == id].to_dict())
+                print(f"Modified {len(merged_df)} accounts")
+
+            # delete accounts
+            elif (args.action == "delete"):
+                for id in tqdm(merged_df['id']):
+                    api.delete_account(id=id)
+                print(f"Deleted {len(merged_df)} accounts")
+                
---
+++ transaction_importer.py
@@ -0,0 +1,57 @@
+from api.transaction import Transaction_API
+from importer.alipay import AlipayTransactionImporter
+import argparse
+from tqdm import tqdm
+import pandas as pd
+
+def parse_args(cmdline=None):
+    parser = argparse.ArgumentParser(description="ezBookkeeping Transaction Updater")
+    parser.add_argument("--file", type=str, help="Path to the input file")
+    parser.add_argument("--action", type=str, help="Action to perform", choices=["add", "list", "delete", "modify", "list-category", "list-tag"])
+    parser.add_argument("--importer", type=str, help="Importer to use", choices=["alipay"], default="alipay")
+    args = parser.parse_args(cmdline)
+    
+    # no need for importer if list-category or list-tag
+    if (args.action == "list-category") or (args.action == "list-tag"):
+        args.importer = None
+    return args
+
+if __name__ == "__main__":
+    # parse arguments
+    args = parse_args()
+    
+    # create api
+    api = Transaction_API()
+    
+    # create importer
+    if args.importer == "alipay":
+        importer = AlipayTransactionImporter()
+        query_transactions = importer.import_transactions(args.file)
+            
+    # add accounts
+    if args.action == "add":
+        for transaction in tqdm(query_transactions):
+            response = api.add_transaction(**transaction.to_dict())
+            if response["success"] != True: 
+                print(transaction.to_dict())
+                raise Exception(response)
+        print(f"Added {len(query_transactions)} transactions")
+        
+    # list & modify & delete transactions
+    elif (args.action == "list") or (args.action == "modify") or (args.action == "delete"):
+        response = api.list_transactions(count=50)
+        ## convert to dataframe
+        result = response['result']
+        result_df = pd.DataFrame(result)
+        
+        # list accounts
+        if (args.action == "list"):
+            print(result_df.loc[:, ['name', 'balance', 'currency']])
+            
+    elif args.action == "list-category":
+        response = api.list_transaction_categories()
+        print(response)
+        
+    elif args.action == "list-tag":
+        response = api.list_transaction_tags()
+        print(response)
